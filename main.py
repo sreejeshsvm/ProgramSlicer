e8e174271187fe694d653c37d64a41632fb563d9
Sreejesh S <sreejeshsvm@gmail.com>
2020-07-02 13:08:27 +0530

    program code readed from a local file

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 if __name__ == '__main__':
 	print("hello world")
+
+	#reading a program file from the folder
+	program_file = open('program_files\\test.py', 'r')
+	program_code = program_file.read()
+	print(program_code)
